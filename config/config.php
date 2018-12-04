e44ed3ac25c92556906bb88e6899e98184526075
inoegraha <45385971+inoegraha@users.noreply.github.com>
2018-12-04 12:51:53 +0700

    rubah encrypt key

--- config/config.php
+++ config/config.php
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'fiesto_car_agente_liber_parallel_paradise';
 
 /*
 |--------------------------------------------------------------------------
