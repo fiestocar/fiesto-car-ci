3bea0b460e0f2e92c742fd6ad03d1bc0167b4869
inoegraha <45385971+inoegraha@users.noreply.github.com>
2018-12-04 12:34:55 +0700

    Add files via upload

---
+++ views/view_dashboard.php
@@ -0,0 +1,12 @@
+<ul>
+    <li><?php echo anchor('kategori_anggota','Kategori Anggota'); ?></li>
+    <li><?php echo anchor('jenis','Jenis Mobil'); ?></li>
+    <li><?php echo anchor('wilayah','Wilayah Operasi') ?></li>
+    <li><?php echo anchor('kondisi','Kondisi Mobil') ?></li>
+    <li><?php echo anchor('denda','Jenis Denda') ?></li>
+    <li><?php echo anchor('anggota','Data Anggota') ?></li>
+    <li><?php echo anchor('mobil','Data Mobil') ?></li>
+    <li><?php echo anchor('penyewaan','Transaksi Penyewaan') ?></li>
+    <li><?php echo anchor('detail_transaksi','Detail Transaksi') ?></li>
+    <li><?php echo anchor('pengembalian','Transaksi Pengembalian') ?></li>
+</ul>
