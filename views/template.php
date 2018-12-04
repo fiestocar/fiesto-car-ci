350dac2e9e86cc2f8bc76ce237cff5582a458bf8
inoegraha <45385971+inoegraha@users.noreply.github.com>
2018-12-05 06:45:03 +0700

    Update template.php

--- views/template.php
+++ views/template.php
@@ -1,118 +1,86 @@
 
 <!doctype html>
-<html class="no-js" lang="en">
+<html lang="en">
 
+<!-- Mirrored from getbootstrap.com/docs/4.1/examples/jumbotron/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 07 Nov 2018 23:42:00 GMT -->
+<meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="https://getbootstrap.com/favicon.ico">
+
     <title>Fiesto Car Dashboard</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/png" href=" <?php echo base_url(); ?>assets/images/icon/favicon.ico">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/themify-icons.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/metisMenu.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/slicknav.min.css">
-    <!-- amchart css -->
-    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
-    <!-- others css -->
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/typography.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/default-css.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/styles.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/responsive.css">
-    <!-- modernizr css -->
-    <script src="<?php echo base_url(); ?>assets/js/modernizr-2.8.3.min.js"></script>
-</head>
 
-<body>
-    <!--[if lt IE 8]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-    <!-- preloader area start -->
-    <div id="preloader">
-        <div class="loader"></div>
-    </div>
-    <!-- preloader area end -->
-    <!-- page container area start -->
-    <div class="page-container">
-        <!-- sidebar menu area start -->
-        <div class="sidebar-menu">
-            <div class="sidebar-header">
-              <h3>Fiesto Car</h3>
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url() ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="jumbotron.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <?php echo anchor('dashboard','Fiesto Car Dashboard',array('class'=>'navbar-brand')); ?>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <!-- <li class="nav-item">
+            <?php echo anchor('kategori','Kategori Barang',array('class'=>'nav-link')); ?>
+          </li>
+          <li class="nav-item">
+            <?php echo anchor('barang','Data Barang',array('class'=>'nav-link')); ?>
+          </li>
+          <li class="nav-item">
+            <?php echo anchor('operator','Operator Sistem',array('class'=>'nav-link')); ?>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="http://example.com/" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Transaksi</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <?php echo anchor('transaksi','Form Transaksi',array('class'=>'dropdown-item')); ?>
+              <?php echo anchor('transaksi/laporan','Laporan Transaksi',array('class'=>'dropdown-item')); ?>
             </div>
-            <div class="main-menu">
-                <div class="menu-inner">
-                    <nav>
-                        <ul class="metismenu" id="menu">
-                            <li >
-                              <a href="<?php site_url() ?>/fiesto_car/dashboard" aria-expanded="true"><i class="ti-dashboard"></i>
-                                  <span>Dashboard</span></a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-table"></i>
-                                    <span>Tabel</span></a>
-                                <ul class="collapse">
-                                  <li><?php echo anchor('kategori_anggota','Kategori Anggota'); ?></li>
-                                  <li><?php echo anchor('jenis','Jenis Mobil'); ?></li>
-                                  <li><?php echo anchor('wilayah','Wilayah Operasi') ?></li>
-                                  <li><?php echo anchor('kondisi','Kondisi Mobil') ?></li>
-                                  <li><?php echo anchor('denda','Daftar Denda') ?></li>
-                                  <li><?php echo anchor('anggota','Daftar Anggota') ?></li>
-                                  <li><?php echo anchor('mobil','Daftar Mobil') ?></li>
-                                </ul>
-                            </li>
-                            <li><a href=#><i class="fa fa-sign-out"></i> <span>logout</span></a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
-        <!-- sidebar menu area end -->
-        <!-- main content area start -->
+          </li> -->
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <?php echo anchor('auth/logout','Logout',array('class'=>'btn btn-outline-secondary')); ?>
+        </form>
+      </div>
+    </nav>
+
+    <main role="main">
+
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron">
         <div class="container">
-
-          <?php echo $contents; ?>
-
+            <?php echo $contents; ?>
         </div>
-        <!-- main content area end -->
-        <!-- footer area start-->
-        <footer>
+      </div>
 
-        </footer>
-        <!-- footer area end-->
-    </div>
-    <!-- page container area end -->
-    <!-- offset area start -->
+      <div class="container">
 
-    <!-- offset area end -->
-    <!-- jquery latest version -->
-    <script src="<?php echo base_url(); ?>assets/js/vendor/jquery-2.2.4.min.js"></script>
-    <!-- bootstrap 4 js -->
-    <script src="<?php echo base_url(); ?>assets/js/popper.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/owl.carousel.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/metisMenu.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/jquery.slimscroll.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/jquery.slicknav.min.js"></script>
+      </div> <!-- /container -->
 
-    <!-- start chart js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
-    <!-- start highcharts js -->
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <!-- start zingchart js -->
-    <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
-    <script>
-    zingchart.MODULESDIR = "https://cdn.zingchart.com/modules/";
-    ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "ee6b7db5b51705a13dc2339db3edaf6d"];
-    </script>
-    <!-- all line chart activation -->
-    <script src="<?php echo base_url(); ?>assets/js/line-chart.js"></script>
-    <!-- all pie chart -->
-    <script src="<?php echo base_url(); ?>assets/js/pie-chart.js"></script>
-    <!-- others plugins -->
-    <script src="<?php echo base_url(); ?>assets/js/plugins.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/scripts.js"></script>
-</body>
+    </main>
 
+    <footer class="container">
+
+      <!-- <p>&copy; Company 2017-2018</p> -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="<?php echo base_url() ?>assets/js/jquery-2.2.4.min.js" ></script>
+    <script>window.jQuery || document.write('<script src="<?php echo base_url() ?>assets/js/jquery-2.2.4.min.js"><\/script>')</script>
+    <script src="../../assets/js/popper.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+
+<!-- Mirrored from getbootstrap.com/docs/4.1/examples/jumbotron/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 07 Nov 2018 23:42:01 GMT -->
 </html>
